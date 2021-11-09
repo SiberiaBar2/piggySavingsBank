31943e46b935bbf309d24bb16cf564d21bfbf273
wind <1215204550@qq.com>
2021-11-09 23:15:29 +0800

    修剪代码

--- src/page/Index/SignPage/index.tsx
+++ src/page/Index/SignPage/index.tsx
@@ -7,14 +7,41 @@ interface SignProps {
   signState?: Array<object>,
   signUpdateMethod?: () => void
 }
-const SignPage: React.FC<SignProps> = ({
-  status,
-  signState,
-  signUpdateMethod
-}) => {
-
+const SignPage: React.FC<SignProps> = (props) => {
+  const {
+    status,
+    signState,
+    signUpdateMethod
+  } = props
+  console.log('signState', signState)
   const toSign = (index: number, returnToken: number) => {
     signUpdateMethod({ status, index, returnToken })
+  }
+  const renderBox = (type, state, index, returnToken) => {
+    return (
+      <div
+        key={index}
+        className={cx(
+          type === 'signWrap'
+            ? 'signWrap'
+            : 'SevenSignWrap')}
+        onClick={
+          state === 2
+            ? () => toSign(index, returnToken)
+            : null}>
+        <span>{returnToken}</span>
+        <span> {
+          state === 2
+            ? test[0]
+            : (state === 0 || state === 1)
+              ? test[2]
+              : state === 3
+                ? test[1]
+                : null}
+        </span>
+        {state === 3 && <div className={cx('rgbaCover')}></div>}
+      </div>
+    )
   }
   return (
     <div className={cx('sign')}>
@@ -24,56 +51,53 @@ const SignPage: React.FC<SignProps> = ({
       <div className={cx('rightContent')}>
         {
           signState.map((item, index) => {
+            const { state, returnToken } = item
             if (index !== 6) {
-              if (item.state === 2) {
-                return <div
-                  key={index}
-                  className={cx('signWrap')}
-                  onClick={() => toSign(index, item.returnToken)}>
-                  <span>{item.returnToken}</span>
-                  <span>{test[0]}</span>
-                </div>
-              } else {
-                return <div
-                  key={index}
-                  className={cx('signWrap')}>
-                  <span>{item.returnToken}</span>
-                  {item.state === 0 || item.state === 0 &&
-                    <span>{test[2]}</span>
-                  }
-                  {item.state === 3 &&
-                    <>
-                      <span>{test[1]}</span>
-                      <div className={cx('rgbaCover')}></div>
-                    </>
-                  }
-                </div>
-              }
+              // if (item.state === 2) {
+              // 必须return!
+              return renderBox('signWrap', state, index, returnToken)
+              // } else {
+              //   return <div
+              //     key={index}
+              //     className={cx('signWrap')}>
+              //     <span>{item.returnToken}</span>
+              //     {(item.state === 0 || item.state === 1) &&
+              //       <span>{test[2]}</span>
+              //     }
+              //     {item.state === 3 &&
+              //       <>
+              //         <span>{test[1]}</span>
+              //         <div className={cx('rgbaCover')}></div>
+              //       </>
+              //     }
+              //   </div>
+              // }
             } else {
-              if (item.state === 2) {
-                return <div
-                  key={index}
-                  className={cx('SevenSignWrap')}
-                  onClick={() => toSign(index, item.returnToken)}>
-                  <span>{item.returnToken}</span>
-                  <span>{test[0]}</span>
-                </div>
-              } else {
-                return <div
-                  key={index}
-                  className={cx('SevenSignWrap')}>
-                  <span>{item.returnToken}</span>
-                  {item.state === 0 || item.state === 0 &&
-                    <span>{test[2]}</span>
-                  }
-                  {item.state === 3 &&
-                    <>
-                      <span>{test[1]}</span>
-                      <div className={cx('rgbaCover')}></div>
-                    </>
-                  }
-                </div>
-              }
+              return renderBox('SevenSignWrap', state, index, returnToken)
+              // if (item.state === 2) {
+              //   return <div
+              //     key={index}
+              //     className={cx('SevenSignWrap')}
+              //     onClick={() => toSign(index, item.returnToken)}>
+              //     <span>{item.returnToken}</span>
+              //     <span>{test[0]}</span>
+              //   </div>
+              // } else {
+              //   return <div
+              //     key={index}
+              //     className={cx('SevenSignWrap')}>
+              //     <span>{item.returnToken}</span>
+              //     {(item.state === 0 || item.state === 1) &&
+              //       <span>{test[2]}</span>
+              //     }
+              //     {item.state === 3 &&
+              //       <>
+              //         <span>{test[1]}</span>
+              //         <div className={cx('rgbaCover')}></div>
+              //       </>
+              //     }
+              //   </div>
+              // }
             }
           })
         }
--- src/page/Index/config/index.ts
+++ src/page/Index/config/index.ts
@@ -11,140 +11,137 @@ export const piggyIcon = [brassPig, sliverPig, glodPig]
 
 export const otherIcon = [piggyToken, glodFruit]
 
-const brassSignList: {
+interface signList {
   state: number,
   returnToken: number
-}[] = [
-    {
-      state: 0,
-      returnToken: 600
-    },
-    {
-      state: 0,
-      returnToken: 600
-    },
-    {
-      state: 0,
-      returnToken: 600
-    },
-    {
-      state: 0,
-      returnToken: 1000
-    },
-    {
-      state: 0,
-      returnToken: 1000
-    },
-    {
-      state: 0,
-      returnToken: 1000
-    },
-    {
-      state: 0,
-      returnToken: 1700
-    },
-  ]
-
-const sliverSignList: {
-  state: number,
-  returnToken: number
-}[] = [
-    {
-      state: 0,
-      returnToken: 1100
-    },
-    {
-      state: 0,
-      returnToken: 1100
-    },
-    {
-      state: 0,
-      returnToken: 1100
-    },
-    {
-      state: 0,
-      returnToken: 1500
-    },
-    {
-      state: 0,
-      returnToken: 1500
-    },
-    {
-      state: 0,
-      returnToken: 1500
-    },
-    {
-      state: 0,
-      returnToken: 1700
-    },
-  ]
-
-const glodSignList: {
-  state: number,
-  returnToken: number
-}[] = [
-    {
-      state: 2,
-      returnToken: 1500
-    },
-    {
-      state: 2,
-      returnToken: 1500
-    },
-    {
-      state: 2,
-      returnToken: 1500
-    },
-    {
-      state: 2,
-      returnToken: 2500
-    },
-    {
-      state: 2,
-      returnToken: 2500
-    },
-    {
-      state: 2,
-      returnToken: 2500
-    },
-    {
-      state: 2,
-      returnToken: 3000
-    },
-  ]
-
-export const piggyList: {
+}
+interface piggySate {
   name: string,
   price: number,
   produce: number,
   text: string,
-  signState: typeof brassSignList
-}[] = [
-    {
-      name: '黄铜储蓄罐',
-      price: 2000,
-      produce: 6500,
-      text: '储蓄币份七天给予',
-      signState: brassSignList
-    },
-    {
-      name: '白银储蓄罐',
-      price: 4000,
-      produce: 9500,
-      text: '储蓄币份七天给予',
-      signState: sliverSignList
-    },
-    {
-      name: '黄金储蓄罐',
-      price: 9000,
-      produce: 15000,
-      text: '储蓄币份七天给予',
-      signState: glodSignList
-    }
-  ]
+  signState: signList[] // typeof brassSignList
+}
+
+const brassSignList: signList[] = [
+  {
+    state: 0,
+    returnToken: 600
+  },
+  {
+    state: 0,
+    returnToken: 600
+  },
+  {
+    state: 0,
+    returnToken: 600
+  },
+  {
+    state: 0,
+    returnToken: 1000
+  },
+  {
+    state: 0,
+    returnToken: 1000
+  },
+  {
+    state: 0,
+    returnToken: 1000
+  },
+  {
+    state: 0,
+    returnToken: 1700
+  },
+]
+
+const sliverSignList: signList[] = [
+  {
+    state: 0,
+    returnToken: 1100
+  },
+  {
+    state: 0,
+    returnToken: 1100
+  },
+  {
+    state: 0,
+    returnToken: 1100
+  },
+  {
+    state: 0,
+    returnToken: 1500
+  },
+  {
+    state: 0,
+    returnToken: 1500
+  },
+  {
+    state: 0,
+    returnToken: 1500
+  },
+  {
+    state: 0,
+    returnToken: 1700
+  },
+]
+
+const glodSignList: signList[] = [
+  {
+    state: 2,
+    returnToken: 1500
+  },
+  {
+    state: 2,
+    returnToken: 1500
+  },
+  {
+    state: 2,
+    returnToken: 1500
+  },
+  {
+    state: 2,
+    returnToken: 2500
+  },
+  {
+    state: 2,
+    returnToken: 2500
+  },
+  {
+    state: 2,
+    returnToken: 2500
+  },
+  {
+    state: 2,
+    returnToken: 3000
+  },
+]
+
+export const piggyList: piggySate[] = [
+  {
+    name: '黄铜储蓄罐',
+    price: 2000,
+    produce: 6500,
+    text: '储蓄币份七天给予',
+    signState: brassSignList
+  },
+  {
+    name: '白银储蓄罐',
+    price: 4000,
+    produce: 9500,
+    text: '储蓄币份七天给予',
+    signState: sliverSignList
+  },
+  {
+    name: '黄金储蓄罐',
+    price: 9000,
+    produce: 15000,
+    text: '储蓄币份七天给予',
+    signState: glodSignList
+  }
+]
 
 
-export const bugStatus = 0
+export const bugStatus = 3
 
 // export const enum piggyTextType {
 //   brassPiggy,
